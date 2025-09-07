222b6c52fe447fc5a45ed6fd9a120ac61c00b927
ZOHOUNVO Brice <CTO.BZOHOUNVO@cma-cgm.com>
2025-09-07 12:13:43 +0100

    Actualisation des codes et finalisation du projet

--- script.js
+++ script.js
@@ -14,24 +14,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //Pour la partie Contact
 //Gestion du formulaire
-const form = document.getlementById('contactForm');
+const form = document.getElementById('contactForm');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
+
   //Validation simple
   if (!form.checkValidity()) {
     alert('Veuillez remplir tous les champs correctement.');
     return;
   }
   //Récupérer les valeurs
-  const prename = form.prename.value.trim();
-  const name = form.name.value.trim();
-  const subject = form.subject.value.trim();
-  const message = form.message.value.trim():
+  const prenom = form.prename.value.trim();
+  const nom = form.name.value.trim();
+  const sujet = form.subject.value.trim();
+  const message = form.message.value.trim();
   //Ici les données peuvent être envoyées via fetch ou autre method
   alert(`|Merci ${prename} ${name} pour votre message sur "${subject}". \n\nMessage:\n${message}`);
   //Reinitialiser le formulaire
   form.reset();
 });
 //Remplir [votre prenom] par un vrai prenom
-document.getElementById('designerLink').textContent = 'VotrePrenom';
+document.getElementById('designerLink').textContent = 'Brice';
 document.getElementById('designerLink').href = 'mailto:zobricol94@gmail.com'
